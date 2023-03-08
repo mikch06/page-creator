0d0662aebbcbf9201329a317672da1f1d4b26d21
mik <mail@kissel.ch>
2023-03-08 22:33:14 +0100

    add iterate-data.py script to create md files out of dummy data

---
+++ scripts/iterate-data.py
@@ -0,0 +1,42 @@
+from csv import DictReader
+import random
+from datetime import datetime
+
+filename = 'wine_content.csv'
+
+with open(filename, 'r') as read_obj:
+    csv_dict_reader = DictReader(read_obj)
+    for col in csv_dict_reader:
+        #print(col['created'], col['title'], col['introtext'], col['catid'])
+        dstamp = datetime.strptime(col['created'], '%Y-%m-%d %H:%M:%S')
+        tstamp = dstamp.strftime("%Y-%m-%d")
+
+        # Random number attach to filename
+        rnmbr = random.randint(1, 99)
+        file = tstamp + '-' + str(rnmbr) + ".md"
+        print("Filename: ", file)
+
+        print("Content:")
+        print("")
+        print("---")
+        print("winename:", col['title'])
+        print("date:", tstamp)
+        print("category:", col['catid'])
+        print("tags:")
+        print("---")
+        print(col['introtext'])
+        #tstamp )
+
+        print("")
+
+        f = open(file, "w")
+        f.write("---\n")
+        f.write("winename: " + col['title'] + "\n")
+        f.write("date: " + tstamp + "\n")
+        f.write("category: " + col['catid'] + "\n")
+        f.write("tags:\n")
+        f.write("---\n")
+        f.write(col['introtext'])
+        f.close()
+
+
