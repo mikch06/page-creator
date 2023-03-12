3fca72dc18d86e7d1d480ad5d10f445c33c68c64
mik <mail@kissel.ch>
2023-03-12 22:17:36 +0100

    POST -> post (small letters)

--- main.py
+++ main.py
@@ -4,36 +4,30 @@ from datetime import datetime
 from jinja2 import Environment, PackageLoader
 from markdown2 import markdown
 
-
-POSTS = {}
+posts = {}
 
 for markdown_post in os.listdir('content'):
     file_path = os.path.join('content', markdown_post)
 
     with open(file_path, 'r') as file:
-        POSTS[markdown_post] = markdown(file.read(), extras=['metadata'])
-
-#
-# POSTS = {
-#     post: POSTS[post] for post in sorted(POSTS, key=lambda post: datetime.strptime(POSTS[post].metadata['date'], '%Y-%m-%d'), reverse=True)
-# }
+        posts[markdown_post] = markdown(file.read(), extras=['metadata'])
 
 env = Environment(loader=PackageLoader('main', 'templates'))
 home_template = env.get_template('home.html')
 post_template = env.get_template('post.html')
 
-posts_metadata = [POSTS[post].metadata for post in POSTS]
+posts_metadata = [posts[post].metadata for post in posts]
 tags = [post['tags'] for post in posts_metadata]
 home_html = home_template.render(posts=posts_metadata, tags=tags)
 
 with open('output/home.html', 'w') as file:
     file.write(home_html)
 
-for post in POSTS:
-    post_metadata = POSTS[post].metadata
+for post in posts:
+    post_metadata = posts[post].metadata
 
     post_data = {
-        'content': POSTS[post],
+        'content': posts[post],
         'winename': post_metadata['winename'],
         'date': post_metadata['date'],
         'category': post_metadata['category'],
@@ -46,4 +40,3 @@ for post in POSTS:
     os.makedirs(os.path.dirname(post_file_path), exist_ok=True)
     with open(post_file_path, 'w') as file:
         file.write(post_html)
-
